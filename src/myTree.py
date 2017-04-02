36c7efd52c7b41b7a2e23648ba378cbe44fe2dbd
jdb051 <jdb051@bucknell.edu>
2017-04-02 17:12:11 -0400

    Added checkpoint 4 and support code

--- src/myTree.py
+++ src/myTree.py
@@ -3,7 +3,6 @@ This is the file that contains your dicision tree
 """
 
 
-
 class DTreeNode:
     """
     Your Node Class for your dicision tree
@@ -13,11 +12,11 @@ class DTreeNode:
         """
         I am allowing these to be public to manipulate them easy
         """
-        self.key = key
+        self.key = key      
         self.data = data
-        
-
-
+        self.class = None
+        self.edges = None #A dictionary storing the reference to children
+            
 
 class MyDecisionTree:
     """
@@ -43,14 +42,41 @@ class MyDecisionTree:
         """
         pass
 
-    
-    def __recursiveBuild(self, root, xData, yData, maxDepth):
+
+    def _entropy(self, xData, xMap, col):
         """
-        Where you really will build the tree
+        Will be used to calculate the entropy of column (col-relative to xData)
+        xData is a 2D Python list
+        xMap is a dictionary that maps the current column number to the original
+        returns a list of unique elements in the col of xData and entropy of column
         """
         pass
 
+    def _stripRowCol(self, xData, yData, col):
+        """
+        Will strip the column and rows related to the unique elements in col.
+        returns a list of 2DLists.  One for each unique element.
+        """
+        pass
+    
+    def __recursiveBuild(self, root, xData, xMap,  yData, currentDepth, maxDepth):
+        """
+        Where you really will build the tree
+        root - of the current subtree (may not be used)
+        xData - 2D Python List of attributes
+        xMap - Dictionary mapping current rows to those in the original 
+        yData - Output
+        currentDepth - How many times we have already recursed
+        maxDepth - how many level the tree will have at most
+        """
+        pass
 
+    def _walk(self, root, xData, xMap):
+        """
+        Walks the tree 
+        returns the most likely ydata classifier
+        """
+        pass
 
 def testMyDecisionTree():
     """
